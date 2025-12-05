3f867be6e51b18abac57836dca673f1e01b6f345
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-05 19:36:24 +0900

    Title: 다양하게 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251205/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << "Total days in Year" << endl;
+    cout << 365 << endl;
+    cout << "Circumference rate" << endl;
+    cout << fixed;
+    cout.precision(10);
+    cout << 3.1415926535;
+    return 0;
+}
