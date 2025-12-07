498173adbe8167e3ad0b9062766386d555e602b3
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-08 08:50:39 +0900

    Title: 세 수의 합과 평균 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251208/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    cout << A + B + C << endl;
+    cout << (A + B + C) / 3;
+
+    return 0;
+}
