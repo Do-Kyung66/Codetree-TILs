fc9958f461682f22ee5604b7cda38b01b0a2b03e
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-10 00:30:36 +0900

    Title: 두 번의 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/두 번의 연산/two-operations.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A;
+    cin >> A;
+    if(A % 2 == 1)
+    {
+        A += 3;
+    }
+    if (A % 3 == 0)
+    {
+        A /= 3;
+    }
+    cout << A;
+    return 0;
+}
