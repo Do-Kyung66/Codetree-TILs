ee500c3b035e8170e815dcfab83745a70a90ded7
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-09 23:57:18 +0900

    Title: 2개 중 최대, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251209/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    int C;
+    A > B ? C = A : C = B;
+    cout << C;
+    return 0;
+}
