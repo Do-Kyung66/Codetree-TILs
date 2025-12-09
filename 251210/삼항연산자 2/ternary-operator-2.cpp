b95a3ba99044f1ac52d64cee42262a3a39927fac
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-10 00:02:19 +0900

    Title: 삼항연산자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    char s;
+    a == 1 ? s = 't' : s = 'f';
+    cout << s; 
+    return 0;
+}
