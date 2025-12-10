8a7278053560466ec9f754dedbe23e4890737c6d
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-10 11:58:28 +0900

    Title: 비교 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/비교 연산/comparison-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    cout << (A >= B) << endl;
+    cout << (A > B) << endl;
+    cout << (B >= A) << endl;
+    cout << (B > A) << endl;
+    cout << (A == B) << endl;
+    cout << (A != B) << endl;
+
+    return 0;
+}
