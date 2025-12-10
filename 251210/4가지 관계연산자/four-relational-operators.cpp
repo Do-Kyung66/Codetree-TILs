1128bd200de15f36925fccf2f04b09fad6835a0f
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-10 12:51:50 +0900

    Title: 4가지 관계연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    cout << (A >= B) << endl;
+    cout << (A > B) << endl;
+    cout << (B >= A) << endl;
+    cout << (B > A) << endl;
+    
+    return 0;
+}
