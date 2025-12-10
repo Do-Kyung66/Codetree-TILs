5d162cfbc8af936ebd002339d49d05c5fdac62d4
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-10 23:56:28 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251210/두 사람/two-person.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    char c, d;
+    cin >> a >> c;
+    cin >> b >> d;
+
+    if ((a >= 19 && c == 'M') || (b >= 19 && d == 'M') )
+    {
+        cout << 1;
+    }
+    return 0;
+}
