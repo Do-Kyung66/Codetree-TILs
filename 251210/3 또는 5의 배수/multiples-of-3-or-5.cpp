fbd67776a672e6e980c8b6cdc83bebd22222ce32
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2025-12-10 11:50:18 +0900

    Title: 3 또는 5의 배수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A;
+    cin >> A;
+    if (A % 3 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else 
+    {
+        cout << "NO" << endl;
+    }
+    if (A % 5 == 0)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
