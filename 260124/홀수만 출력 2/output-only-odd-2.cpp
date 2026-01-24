c80e53fd4921d4ef152301541682856fd28eb3be
Do-Kyung66 <136219266+Do-Kyung66@users.noreply.github.com>
2026-01-24 11:22:51 +0900

    Title: 홀수만 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> B >> A;
+    for (int i = B; i >= A; i--)
+    {
+        if(i % 2 == 1)
+        {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
